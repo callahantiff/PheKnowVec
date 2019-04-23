1b0c403984b9c8146134c339e864f6b5819724b6
callahantiff <callahantiff@gmail.com>
2019-04-23 09:40:16 -0600

    cleaned up code and refactored GoogleSheet class

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ from scripts.data_processor import GSProcessor
 
 
 def domain_occurrence(data, url, databases):
-    """Function takes a pandas dataframe and a string containing a url which redirects to a GitHub Gist SQL
-    query. The function generates a query and runs it against the CHCO de-identified and MIMIC-III OMOP database.
+    """Function takes a pandas dataframe and a string containing a url which redirects to a GitHubGist SQL query. The
+    function formats a query and runs it against the input database(s). This function assumes that it will be passed
+    two relational databases as an argument.
 
     Args:
         data: A pandas dataframe.
@@ -33,7 +34,7 @@ def domain_occurrence(data, url, databases):
         print('\n' + '#' * 50 + '\n' + 'Running query against {0}'.format(db_))
         db = GBQ('sandbox-tc', db_)
 
-        # query data
+        # generate and run queries against SQL database
         query_results = []
         batch = data.groupby(np.arange(len(data)) // 15000)
 
@@ -47,35 +48,32 @@ def domain_occurrence(data, url, databases):
         merged = pd.concat(query_results).drop_duplicates()
 
         # rename column
-        merged.rename(columns={'drg_count': str(db_.split('_')[0]) + '_drg_count'}, inplace=True)
+        merged.rename(columns={'_count': str(db_.split('_')[0]) + '_count'}, inplace=True)
         merged_results.append(merged)
 
-    # merge counts results together
-    merge_col1 = set(list(merged_results[0])).intersection(set(list(merged_results[1])))
+    # merge results from the input databases together
+    col_set1 = set(list(merged_results[0])).intersection(set(list(merged_results[1])))
     merged_comb = pd.merge(left=merged_results[0], right=merged_results[1], how='outer',
-                           left_on=list(merge_col1), right_on=list(merge_col1))
+                           left_on=list(col_set1), right_on=list(col_set1))
 
-    # group merged count results by source_code
-    if 'source_string' in list(data):
-        merged_comb_agg1 = merged_comb.fillna(0).groupby(['source_code', 'standard_code'], as_index=False).agg({
-            'CHCO_drg_count': lambda x: sum(set(x)), 'MIMICIII_drg_count': lambda x: sum(set(x))})
-    else:
-        merged_comb_agg1 = merged_comb.fillna(0).groupby('standard_code', as_index=False).agg(
-            {'CHCO_drg_count': lambda x: sum(set(x)), 'MIMICIII_drg_count': lambda x: sum(set(x))})
+    # aggregate merged count results by standard_code
+    db1_name = str(databases[0].split('_')[0]) + '_count'
+    db2_name = str(databases[1].split('_')[0]) + '_count'
+    merged_agg = merged_comb.fillna(0).groupby('standard_code', as_index=False).agg(
+            {str(db1_name): lambda x: sum(set(x)),
+             str(db2_name): lambda x: sum(set(x))})
 
     # combine results with full dataset
-    merge_col2 = set(list(data)).intersection(set(list(merged_comb_agg1)))
-    merged_full = pd.merge(left=data, right=merged_comb_agg1, how='outer',
-                           left_on=list(merge_col2), right_on=list(merge_col2))
+    col_set2 = set(list(data)).intersection(set(list(merged_agg)))
+    merged_full = pd.merge(left=data, right=merged_agg, how='outer', left_on=list(col_set2), right_on=list(col_set2))
 
-    # replace rows with no counts with zero
-    merged_full[['CHCO_drg_count', 'MIMICIII_drg_count']] = merged_full[['CHCO_drg_count',
-                                                                         'MIMICIII_drg_count']].fillna(0)
+    # replace rows with no counts, stored as 'NaN' with zero
+    merged_full[[db1_name, db2_name]] = merged_full[[db1_name, db2_name]].fillna(0)
 
     return merged_full.drop_duplicates()
 
 
-def reg_query(data, input_source, mod, gbq_db, url, query, gbq_database):
+def regular_query(data, input_source, mod, gbq_db, url, query, gbq_database):
     """Function generates a SQL query and runs it against a Google Big Query database. The returned results are
     then used in a second query designed to retrieve
 
@@ -117,12 +115,59 @@ def reg_query(data, input_source, mod, gbq_db, url, query, gbq_database):
         return cont_results
 
 
+def descriptive(data, plot_title, plot_x_axis, plot_y_axis):
+    """Function takes a pandas data frame, and three strings that contain information about the data frame. The
+    function then derives and prints descriptive statistics and histograms.
+
+    Args:
+        data: a pandas dataframe.
+        plot_title: A string containing a title for the histogram.
+        plot_x_axis: A string containing a x-axis label for the histogram.
+        plot_y_axis: A string containing a y-axis label for the histogram.
+
+    Return:
+        None
+    """
+
+    # subset data to only include standard concepts and counts
+    dat_plot = data.copy()
+    plt_dat_chco = dat_plot[['standard_code', 'CHCO_count']].drop_duplicates()
+    plt_dat_mimic = dat_plot[['standard_code', 'MIMICIII_count']].drop_duplicates()
+
+    # get counts of drug occurrences by standard codes
+    # CHCO
+    print(plt_dat_chco[['standard_code']].describe())
+    num_c = plt_dat_chco.loc[plt_dat_chco['CHCO_drg_count'] > 0.0]['CHCO_drg_count'].sum()
+    denom_c = len(set(list(plt_dat_chco.loc[plt_dat_chco['CHCO_drg_count'] > 0.0]['standard_code'])))
+    print('CHCO: There are {0} Unique Standard Codes with Drug Occurrence > 0'.format(denom_c))
+    print('CHCO: The Average Drug Occurrences per Standard Code is : {0} \n'.format(num_c / denom_c))
+
+    # MIMIC
+    print(plt_dat_mimic[['standard_code']].describe())
+    num_m = plt_dat_mimic.loc[plt_dat_mimic['MIMICIII_count'] > 0.0]['MIMICIII_count'].sum()
+    denom_m = len(set(list(plt_dat_mimic.loc[plt_dat_mimic['MIMICIII_count'] > 0.0]['standard_code'])))
+    print('MIMICIII: There are {0} Unique Standard Codes with Drug Occurrence > 0'.format(denom_m))
+    print('MIMICIII: The Average Drug Occurrences per Standard Code is : {0} \n'.format(num_m / denom_m))
+
+    # generate plots
+    f = plt.figure()
+    with sns.axes_style("darkgrid"):
+        f.add_subplot(1, 2, 1)
+        sns.distplot(list(plt_dat_mimic['MIMICIII_count']), color="dodgerblue", label="MIMIC-III Count")
+        plt.legend()
+        plt.ylabel(plot_y_axis)
+        f.add_subplot(1, 2, 2)
+        sns.distplot(list(plt_dat_chco['CHCO_count']), color="red", label="CHCO Count")
+        plt.legend()
+    f.text(0.5, 0.98, plot_title, ha='center',
+           va='center')
+    f.text(0.5, 0.01, plot_x_axis, ha='center', va='center')
+    plt.show()
+
+    return None
+
+
 def main():
-    # # instantiate class and read in phenotypes
-    # all_data = GSProcessor(['Phenotype Definitions', 'ADHD_test'])
-    # all_data.data_download()
-    # data = all_data.get_data().dropna(how='all', axis=1).dropna()
-    # data = data.drop(['cohort', 'criteria', 'phenotype_criteria'], axis=1).drop_duplicates()
 
     ########################
     # GBQ: query an existing table
@@ -145,10 +190,10 @@ def main():
                 ['CSEM_OMOP_KE_Children', '', ['str', 'source_id', 'source_domain', 'SRC_DRG_COUNT', None]],
                 ['CSEM_OMOP_KE_Desc', '', ['str', 'source_id', 'source_domain', 'SRC_DRG_COUNT', None]]]:
 
-        source_results = reg_query(data, dat[2], dat[1], gbq_db, url, dat[0], databases[0])
+        source_results = regular_query(data, dat[2], dat[1], gbq_db, url, dat[0], databases[0])
         print(source_results[['source_string', 'source_code', 'source_vocabulary']].describe())
 
-        # write out source data
+        # write out source data to Google sheet
         sht_new = '{0}_{1}'.format(sht, dat[0])
         all_data.create_worksheet(sht_new)
         all_data.set_worksheet(sht_new)
@@ -165,152 +210,23 @@ def main():
             data2 = data2.drop(['source_string', 'source_domain', 'source_name', 'input_type'],
                                axis=1).drop_duplicates()
 
-            stand_results = reg_query(data2, dat2[2], dat2[1], gbq_db, url, dat2[0], databases[0])
+            stand_results = regular_query(data2, dat2[2], dat2[1], gbq_db, url, dat2[0], databases[0])
 
             # print descriptive stats
-            print(stand_results[['source_code', 'source_vocabulary']].describe())
-            print(stand_results[['standard_code', 'standard_vocabulary']].describe())
+            print(data[['source_code', 'source_vocabulary']].describe())
+            print(data[['standard_code', 'standard_vocabulary']].describe())
 
-            # subset data to only include standard concepts and counts
-            dat_plot = stand_results.copy()
-            plt_dat_chco = dat_plot[['standard_code', 'CHCO_drg_count']].drop_duplicates()
-            plt_dat_mimic = dat_plot[['standard_code', 'MIMICIII_drg_count']].drop_duplicates()
+            # generate histograms and output for occurrence counts
+            descriptive(stand_results,
+                        '{0}_{1}_{2}: Occurrence Counts'.format(sht, dat[0], dat2[0]),
+                        'Drug Exposure Occurrence (Count)',
+                        'Density')
 
-            # get counts of drug occurrences by standard codes
-            print(plt_dat_chco[['standard_code']].describe())
-            num_c = plt_dat_chco.loc[plt_dat_chco['CHCO_drg_count'] > 0.0]['CHCO_drg_count'].sum()
-            denom_c = len(set(list(plt_dat_chco.loc[plt_dat_chco['CHCO_drg_count'] > 0.0]['standard_code'])))
-            print(denom_c)
-            print(num_c / denom_c)
-
-            print(plt_dat_mimic[['standard_code']].describe())
-            num_m = plt_dat_mimic.loc[plt_dat_mimic['MIMICIII_drg_count'] > 0.0]['MIMICIII_drg_count'].sum()
-            denom_m = len(set(list(plt_dat_mimic.loc[plt_dat_mimic['MIMICIII_drg_count'] > 0.0]['standard_code'])))
-            print(denom_m)
-            print(num_m / denom_m)
-
-            f = plt.figure()
-            with sns.axes_style("darkgrid"):
-                f.add_subplot(1, 2, 1)
-                sns.distplot(list(plt_dat_mimic['MIMICIII_drg_count']), color="dodgerblue", label="MIMIC-III Count")
-                plt.legend()
-                plt.ylabel('Density')
-                f.add_subplot(1, 2, 2)
-                sns.distplot(list(plt_dat_chco['CHCO_drg_count']), color="red", label="CHCO Count")
-                plt.legend()
-            f.text(0.5, 0.98, '{0}_{1}_{2}: Drug Occurrence Counts'.format(sht, dat[0], dat2[0]), ha='center',
-                   va='center')
-            f.text(0.5, 0.01, 'Drug Exposure Occurrence (Count)', ha='center', va='center')
-            plt.show()
-
-            # write out standard data
+            # write out standard data to Google sheet
             sht_new = '{0}_{1}_{2}'.format(sht, dat[0], dat2[0])
             all_data.create_worksheet(sht_new)
             all_data.set_worksheet(sht_new)
             gd.set_with_dataframe(all_data.get_worksheet(), stand_results)
-
-    # get source_code mappings
-    # code_url = url[0].split(';')[-1]
-    # sql_code_args = all_data.code_format(data)
-    # code_res = db.gbq_query(code_url, sql_code_args)
-
-    # sht_list = ['ADHD_179', 'Appendicitis_236', 'Crohns Disease_77', 'Hypothyroidism_14', 'Peanut Allergy_609',
-    #             'Sickle Cell Disease_615', 'Sleep Apnea_240', 'Steroid-Induced Osteonecrosis_155',
-    #             'Systemic Lupus Erythematosus_1058']
-    # sht_list = ['ADHD_179', 'Appendicitis_236', 'Crohns Disease_77', 'Hypothyroidism_14', 'Peanut Allergy_609',
-    #               'Sickle Cell Disease_615', 'Sleep Apnea_240', 'Steroid-Induced Osteonecrosis_155',
-    #               'Systemic Lupus Erythematosus_1058']
-    #
-    # for db_name in ('CHCO_DeID_Oct2018', 'MIMICIII_OMOP_Mar2019'):
-    #     print('#' * 100 + '\n')
-    #     print('Processing Database: {0}'.format(db_name))
-    #
-    #     db = GBQ('sandbox-tc', db_name)
-    #     url = {x.split(';')[0]: x.split(';')[1] for x in open('resources/github_gists.txt', 'r').read().split('\n')}
-    #
-    #     for sheet in sht_list:
-    #         if '_' in str(sheet):
-    #             # sht = str(sheet).split("'")[1]
-    #             sht = sheet
-    #
-    #             try:
-    #                 print('#' * 50 + '\n')
-    #                 print('Running Google Sheet: {0}, Tab: {1}'.format('Phenotype Definitions', sht))
-    #
-    #                 all_data = GSProcessor(['Phenotype Definitions', sht])
-    #                 all_data.data_download()
-    #                 data = all_data.get_data().dropna(how='all', axis=1).dropna()
-    #                 data = data.drop(['cohort', 'criteria', 'phenotype_criteria', 'phenotype'],
-    #                 axis=1).drop_duplicates()
-    #
-    #                 print(len(data.groupby('input_type').get_group('Code')))
-    #
-    #                 # get source_string to source_code mappings
-    #                 if 'Drug' in data.groupby('source_domain').groups.keys():
-    #                     print('Running Synonyms Query against Database: {0}'.format(db_name))
-    #                     sql_string_syn = all_data.string_format(data.groupby('source_domain').get_group('Drug'))
-    #                     syn_res = db.gbq_query(url['source_string_syn'], sql_string_syn)
-    #                     data = pd.concat([data, syn_res], axis=0, sort=True).drop_duplicates()
-    #
-    #                 print('Running Query against Database: {0}'.format(db_name))
-    #                 string_url = url['source_string']
-    #                 sql_string_args = all_data.string_format(data)
-    #                 string_res = db.gbq_query(string_url, sql_string_args)
-    #
-    #                 # merge results
-    #                 print('Merging Results')
-    #                 merge_cols = ['source_code', 'source_domain', 'source_string', 'source_domain']
-    #                 keep_cols = ['source_vocabulary', 'source_code', 'source_name']
-    #                 merged_res = all_data.result_merger(data, string_res, merge_cols, keep_cols)
-    #
-    #                 # export merged data to Google Sheet
-    #                 print('\n Writing {0} results to Google Sheet'.format(len(merged_res)))
-    #                 sht_new = str(sht) + '_' + str(db_name).split('_')[0] + '_SrcStr2Code'
-    #                 all_data.create_worksheet(sht_new)
-    #                 all_data.set_worksheet(sht_new)
-    #                 gd.set_with_dataframe(all_data.get_worksheet(), merged_res)
-    #
-    #             except KeyError:
-    #                 print("Skipping worksheet as it contains no Strings to process")
-    #                 pass
-    # for qury in ['stand_terms', 'stand_terms_children', 'stand_terms_desc']:
-    #     string_url = url[qury]
-    #     df_results1 = []
-    #     batch_data1 = data.groupby(np.arange(len(data)) // 15000)
-    #
-    #     for name, group in batch_data1:
-    #         sql_code_args = code_format(group)
-    #         code_res = gbq_query(string_url, (db_[0], *sql_code_args)).drop_duplicates()
-    #         df_results1.append(code_res.drop_duplicates())
-    #
-    #     string_res = pd.concat(df_results1).drop_duplicates()
-    #     print(string_res[['source_code']].describe())
-    #     print(string_res[['source_vocabulary']].describe())
-    #     print(string_res[['standard_code']].describe())
-    #     print(string_res[['standard_vocabulary']].describe())
-    #     # print(string_res[['source_string', 'source_vocabulary']].describe())
-    #
-    #     # get counts of drg_occurrence
-    #     for x in db:
-    #         df_results = []
-    #         batch_data = string_res.groupby(np.arange(len(string_res)) // 15000)
-    #
-    #         for name, group in batch_data:
-    #             sql_drg_args = string_format2(group)
-    #
-    #             if q in []
-    #             drg_res = gbq_query(url["CODE_DRG_COUNT"], (db_[0], *sql_drg_args)).drop_duplicates()
-    #             print(len(drg_res.drop_duplicates()))
-    #             df_results.append(drg_res.drop_duplicates())
-    #
-    #         string_res2 = pd.concat(df_results).drop_duplicates()
-    #
-    #     # export merged data to Google Sheet
-    #     # print('\n Writing {0} results to Google Sheet'.format(len(merged_res)))
-    #     sht_new = 'ADHD_test_' + str(dat) + "_" + str(qury)
-    #     all_data.create_worksheet(sht_new)
-    #     all_data.set_worksheet(sht_new)
-    #     gd.set_with_dataframe(all_data.get_worksheet(), string_res)
 
     # ########################
     # # GBQ: create a new table -- after verifying mappings
--- scripts/data_processor.py
+++ scripts/data_processor.py
@@ -31,7 +31,7 @@ class GSProcessor(object):
         self.sheet_info = sheet_info
         self.sheet = gspread.authorize(self.cred).open(self.sheet_info[0])
         self.worksheet = ''
-        self.data = ''
+        self.data = pd.DataFrame()
 
     def data_download(self):
         """Connects to Google Sheets, downloads a spreadsheet and saves it to a pandas data frame."""
@@ -78,162 +78,12 @@ class GSProcessor(object):
         self.worksheet = self.sheet.worksheet(sheet_name)
         print('Updated, switched to Google Sheet: {0}, Tab: {1}\n'.format(self.sheet_info[0], sheet_name))
 
-    # @staticmethod
-    # def code_format(codes):
-    #     """Extract information needed to in an SQL query from a pandas data frame and return needed information as a
-    #     list of sets. This function assumes that the input data contains specific columns: 'Input_Type',
-    #     'Source_Code', 'Source_Vocabulary', and 'Standard_Vocabulary'.
-    #
-    #     Args:
-    #          codes: A pandas data frame.
-    #
-    #     Returns:
-    #         A tuple of joined sets, where each set contains an SQL parameter. For example:
-    #
-    #         ('"348.1", "348.2", "348.3"', '"ICD9CM"', '"SNOMED"')
-    #
-    #     Raises:
-    #         An error occurs if the data frame does not contain the mandatory columns.
-    #         An error occurs when the input data frame does not contain required data (i.e. source codes,
-    #         source vocab, and standard vocabulary information.
-    #     """
-    #
-    #     if 'input_type' and 'source_code' and 'source_vocabulary' and 'standard_vocabulary' not in list(codes):
-    #         raise ValueError('Error - data does not contain the mandatory columns. Please make sure your data frame '
-    #                          'includes the following columns: input_type, source_code, source_vocabulary, and '
-    #                          'standard_vocabulary')
-    #     else:
-    #
-    #         source_code = set()
-    #         source_vocab = set()
-    #         standard_vocab = set()
-    #
-    #         code_data = codes.groupby('input_type').get_group('Code')
-    #
-    #         start = datetime.now()
-    #         print('\n Started processing source codes: {}'.format(start))
-    #         widgets = [Percentage(), Bar(), FormatLabel('(elapsed: %(elapsed)s)')]
-    #         pbar = ProgressBar(widgets=widgets, maxval=len(code_data))
-    #
-    #         for row, val in pbar(code_data.iterrows()):
-    #             source_code.add(val['source_code'])
-    #             source_vocab.add(val['source_vocabulary'])
-    #             standard_vocab.add(val['standard_vocabulary'])
-    #
-    #         pbar.finish()
-    #         finish = datetime.now()
-    #         print("Finished processing source codes: {}".format(finish))
-    #
-    #         # verify we have results
-    #         if not len(source_code) and len(source_vocab) and len(standard_vocab) >= 1:
-    #             raise ValueError('Error - check your data file, important variables may be missing')
-    #         else:
-    #             duration = finish - start
-    #             time_diff = round(duration.total_seconds(), 2)
-    #             print('Processed {0} codes in {1} seconds \n'.format(len(source_code), time_diff))
-    #
-    #             return (','.join(map(str, source_code)),
-    #                     '"' + '","'.join(map(str, source_vocab)) + '"',
-    #                     '"' + '","'.join(map(str, standard_vocab)) + '"')
-    #
-    # @staticmethod
-    # def string_format(strings):
-    #     """Extract information needed to in an SQL query from a pandas data frame and return needed information as a
-    #     list of sets. This function assumes that the input data contains specific columns: 'Input_Type',
-    #     'Source_Code', 'Source_Vocabulary', and 'Standard_Vocabulary'.
-    #
-    #     Args:
-    #          strings: A pandas data frame.
-    #
-    #     Returns:
-    #         A tuple of joined sets, where each set contains an SQL parameter. For example:
-    #
-    #         ('"WHEN lower(concept_name) LIKE '%clonazepam%' THEN '%clonazepam%'', '"Drug"')
-    #
-    #     Raises:
-    #         An error occurs if the data frame does not contain the mandatory columns.
-    #         An error occurs when the input data frame does not contain required data (i.e. source strings and source
-    #         domain information.
-    #     """
-    #     source_string = []
-    #     source_domain = set()
-    #
-    #     if 'input_type' and 'source_domain' and 'source_code' not in list(strings):
-    #         raise ValueError('Error - data does not contain the mandatory columns. Please make sure your data frame '
-    #                          'includes the following columns: input_type, source_domain, and source_code')
-    #     else:
-    #
-    #         string_data = strings.groupby('input_type').get_group('String')
-    #
-    #         start = datetime.now()
-    #         print('\n Started processing source strings: {}'.format(start))
-    #         widgets = [Percentage(), Bar(), FormatLabel('(elapsed: %(elapsed)s)')]
-    #         pbar = ProgressBar(widgets=widgets, maxval=len(string_data))
-    #
-    #         for row, val in pbar(string_data.iterrows()):
-    #             source_domain.add(val['source_domain'])
-    #             # med = val['source_code'].strip('"').lower()
-    #             # source_string.append("WHEN lower(c.concept_name) LIKE '%{0}%' THEN '{0}'".format(med))
-    #             med = val['source_code'].lower()
-    #             source_string.append("WHEN lower(c.concept_name) LIKE {0} THEN '{0}'".format(med))
-    #
-    #         pbar.finish()
-    #         finish = datetime.now()
-    #         print("Finished processing source strings: {}".format(finish))
-    #
-    #         # verify we have results
-    #         if not len(source_string) and len(source_domain) >= 1:
-    #             raise ValueError('Error - check your data file, important variables may be missing')
-    #         else:
-    #             duration = finish - start
-    #             time_diff = round(duration.total_seconds(), 2)
-    #             print('Processed {0} strings in {1} seconds \n'.format(len(source_string), time_diff))
-    #
-    #             return '\n '.join(map(str, set(source_string))), '"' + '","'.join(map(str, source_domain)) + '"'
-
-    @staticmethod
-    def result_merger(data, results, merge_cols, keep_cols):
-        """Process results of querying Google Big Query and merge them with data extracted from a Google Sheet.
-
-        Args:
-            data: A pandas data frame containing data from a Google Sheet.
-            results: A pandas data frame containing data from running a query against GBQ.
-            merge_cols: A list of columns to merge results and data with.
-            keep_cols: A list of columns to keep in the resulting merged data frame.
-
-        Return:
-            A pandas data frame containing the original data merged with the results from running a query against GBQ.
-        """
-
-        # merge strings to existing data
-        merged_data = pd.merge(left=data, right=results, how='left',
-                               left_on=[merge_cols[0], merge_cols[1]],
-                               right_on=[merge_cols[2], merge_cols[3]])
-
-        for row, val in merged_data.iterrows():
-            if val['input_type'] == 'String' and pd.isnull(val[str(keep_cols[1]) + '_y']) is False:
-                val[str(keep_cols[0]) + '_x'] = val[str(keep_cols[0]) + '_y']
-                val[str(keep_cols[1]) + '_x'] = val[str(keep_cols[1]) + '_y']
-                val[str(keep_cols[2]) + '_x'] = val[str(keep_cols[2]) + '_y']
-
-        # rename columns
-        merged_data.rename(columns={str(keep_cols[0]) + '_x': str(keep_cols[0]).split('_x')[0],
-                                    str(keep_cols[1]) + '_x': str(keep_cols[1]).split('_x')[0],
-                                    str(keep_cols[2]) + '_x': str(keep_cols[2]).split('_x')[0]}, inplace=True)
-
-        # drop old columns
-        merged_data = merged_data.drop([x for x in list(merged_data) if '_y' in x], axis=1)
-
-        return merged_data
-
-    @staticmethod
-    def code_format(data, input_source, mod=''):
+    def code_format(self, input_source, mod=''):
         """Extract information needed to in an SQL query from a pandas data frame and return needed information as a
         list
         of sets.
 
         Args:
-            data: A pandas dataframe.
             input_source: A list of strings that represent columns in a pandas dataframe. The function assumes that
                 the list contains the following:
                     (1) a string that indicates if the query uses input strings or codes
@@ -257,22 +107,23 @@ class GSProcessor(object):
             An error occurs when the input data frame does not contain required data (i.e. source codes, source vocab,
             and standard vocabulary information.
         """
+        temp_data = self.get_data().copy()
 
         if 'code' not in input_source[0] and mod == '':
             format_nomod = lambda x: "WHEN lower(c.concept_name) LIKE '{0}' THEN '{0}'".format(x.strip('"').lower())
-            source1 = set(list(data[str(input_source[1])].apply(format_nomod)))
-            source2 = set(list(data[str(input_source[2])]))
+            source1 = set(list(temp_data[input_source[1]].apply(format_nomod)))
+            source2 = set(list(temp_data[input_source[2]]))
             res = '\n'.join(map(str, source1)), '"' + '","'.join(map(str, source2)) + '"'
 
         elif 'code' not in input_source[0] and mod != '':
             format_mod = lambda x: "WHEN lower(c.concept_name) LIKE '%{0}%' THEN '{0}'".format(x.strip('"').lower())
-            source1 = set(list(data[str(input_source[1])].apply(format_mod)))
-            source2 = set(list(data[str(input_source[2])]))
+            source1 = set(list(temp_data[input_source[1]].apply(format_mod)))
+            source2 = set(list(temp_data[input_source[2]]))
             res = '\n'.join(map(str, source1)), '"' + '","'.join(map(str, source2)) + '"'
 
         else:
-            source1 = set(list(data[str(input_source[1])]))
-            source2 = set(list(data[str(input_source[2])]))
+            source1 = set(list(temp_data[input_source[1]]))
+            source2 = set(list(temp_data[input_source[2]]))
             res = ','.join(map(str, source1)), '"' + '","'.join(map(str, source2)) + '"'
 
         if not len(source1) >= 1:
